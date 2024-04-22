3309514e66c926994792bf554a3120b4f191fbc1
Neriyah Butler <neriyah.butler21@gmail.com>
2024-04-22 17:29:27 -0500

    Created layers of API and added "User Settings" endpoints to controller file

--- FFA User Settings API/Controllers/FFAUserSettingsController.cs
+++ FFA User Settings API/Controllers/FFAUserSettingsController.cs
@@ -2,6 +2,8 @@
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
+// TODO -- Go through the swagger file we have for the API and ensure the summary and etc is accurate
+
 namespace FFA_User_Settings_API.Controllers
 {
     [Route("api/[controller]")]
@@ -15,7 +17,37 @@ namespace FFA_User_Settings_API.Controllers
             _ffaUserSettingsHandler = ffaUserSettingsHandler;
         }
 
-        [HttpGet]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<UserSettingsAPIModel>))]
+        // User Settings Endpoints
+
+        [HttpGet("/UserSettings/{username}")]
+        [ProducesResponseType(200, Type = typeof(UserSettingsAPIModel))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetUserSettings(string username) 
+            // NOTES FOR SELF:
+            // An action result is the result of a controller action's execution in response to a browser request
+            // One question I have though, why do we have to do "Task<IActionResult>" ?
+            // I need to learn more about Task and ActionResult/IActionResult
+        {
+            var userSettings = await _ffaUserSettingsHandler.GetUserSettings(username);
+            return Ok(userSettings);
+        }
+
+        [HttpPost("/UserSettings")]
+        [ProducesResponseType(200, Type = typeof(UserSettingsAPIModel))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> InsertUserSettings(UserSettingsAPIModel? userSettingsInput)
+        {
+            var userSettings = await _ffaUserSettingsHandler.InsertUserSettings(userSettingsInput);
+            return Ok(userSettings);
+        }
+
+        [HttpPatch("/UserSettings/{username}")]
+        [ProducesResponseType(200, Type = typeof(UserSettingsAPIModel))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> UpdateUserSettings(string username,  UserSettingsAPIModel? userSettingsInput)
+        {
+            var userSettings = await _ffaUserSettingsHandler.UpdateUserSettings(username, userSettingsInput);
+            return Ok(userSettings);
+        }
     }
 }
--- FFA User Settings API/Program.cs
+++ FFA User Settings API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+builder.Services.AddScoped<IFFAUserSettingsHandler, IFFAUserSettingsHandler>();
 
 app.UseHttpsRedirection();
 
